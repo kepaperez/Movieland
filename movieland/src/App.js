d4d4dc90995f73f5f94ed977942f3de36fbd3873
kepa <57668544+kepaperez@users.noreply.github.com>
2022-04-03 00:45:47 +0200

    Movieland start

--- movieland/src/App.js
+++ movieland/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import  {useEffect} from 'react';
+import'./App.css';
+import SearchIcon from './search.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//54:00
+
+const API_URP ='https://www.omdbapi.com?apikey=12b6a11c';
+const App = () =>{
+
+    const searchMovies = async (title) =>{
+        const response = await fetch(`${API_URP}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+    useEffect(() =>{
+        searchMovies('Spiderman');
+
+    },[]);
+    return(
+        <div className='app'>
+              <h1>Movieland</h1>
+
+              <div className='search'>
+                  <input placeholder='Search something'
+                  value="Superman"
+                  />
+              </div>
+        </div>
+      
+    );
+
 }
 
 export default App;

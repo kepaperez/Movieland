ce271a99341200cc823a5ced14451d6bb15a0e2e
kepa <57668544+kepaperez@users.noreply.github.com>
2022-04-04 20:27:38 +0200

    movie end tutorial

--- movieland/src/App.js
+++ movieland/src/App.js
@@ -1,33 +1,58 @@
-import  {useEffect} from 'react';
-import'./App.css';
+import { useEffect, useState } from 'react';
+import './App.css';
 import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-//54:00
+import Movie from './movie';
 
-const API_URP ='https://www.omdbapi.com?apikey=12b6a11c';
-const App = () =>{
+const API_URP = 'https://www.omdbapi.com?apikey=12b6a11c';
 
-    const searchMovies = async (title) =>{
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const[searchTerm,setSearchTerm] = useState([]);
+
+    const searchMovies = async (title) => {
         const response = await fetch(`${API_URP}&s=${title}`);
+        
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
-    useEffect(() =>{
-        searchMovies('Spiderman');
-
-    },[]);
-    return(
+    useEffect(() => {
+        searchMovies('Batman');
+    }, []);
+    return (
         <div className='app'>
-              <h1>Movieland</h1>
+            <h1>Movieland</h1>
 
-              <div className='search'>
-                  <input placeholder='Search something'
-                  value="Superman"
-                  />
-              </div>
+            <div className='search'>
+                <input placeholder='Search something'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value) }
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+            {
+                movies?.length > 0
+                    ? (
+                        <div className='container'>
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie} />
+                            ))}
+
+                        </div>
+                    ) : (
+                        <div className='empty'>
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+            }
+
         </div>
-      
     );
 
 }
---
+++ movieland/src/movie.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+
+const Movie = () => {
+  
+    return (
+        <div>
+            <h1>Movie</h1>
+        </div>
+       
+    );
+
+}
+
+export default Movie;
